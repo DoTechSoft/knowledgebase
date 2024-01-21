715d328871b7dab6037a30c1252a8dc01938f007
mirsuhail029 <mirsuhail029@gmail.com>
2024-01-21 19:14:31 +0530

    created the header section and fetchquestion and fetchtestpaper scripts and the basic structure for questions and fetching all the data from questionbank json file which will be replaced by an API in future

---
+++ README.md
@@ -0,0 +1,2 @@
+#KnowledgeBase - A Vanilla JavaScript Project
+A competitive exam application built using HTML, CSS and JavaScript for front-end and PHP for Back-end.
---
+++ script.js
@@ -0,0 +1,88 @@
+import fetchTestPaper from "./scripts/fetchTestPaper.js";
+import fetchQuestion from "./scripts/fetchQuestion.js";
+
+const card = document.getElementById("card-contents");
+const submitButton = document.getElementById("q1");
+const stat = document.getElementById("stat");
+const nextButton = document.getElementById("next");
+const finishButton = document.getElementById("finish");
+const questionElement = document.getElementById("question");
+const summary = document.getElementById("summary");
+const summaryStats = document.querySelectorAll("#summaryStats :nth-child(odd)");
+const summaryMessage = document.querySelectorAll(
+  "#summaryMessage :nth-child(odd)"
+);
+
+async function run() {
+  const testPaper = await fetchTestPaper();
+
+  let questionCounter = 0;
+  const generateQuestion = await fetchQuestion(
+    testPaper,
+    questionCounter,
+    questionElement
+  );
+  // nextButton.setAttribute("disabled", "true");
+  // submitButton.removeAttribute("disabled");
+  // card.setAttribute("class", "card-contents");
+  // stat.textContent = "Good Luck";
+
+  nextButton.addEventListener("click", generateQuestion);
+
+  // function for generating the final score and displaying it onto the screen
+  let finalScore = 0;
+  const [correct, total, score] = summaryStats;
+  const [alert, conclusion] = summaryMessage;
+  const summaryFunction = function () {
+    const grade = finalScore / testPaper.length >= 0.4;
+    card.setAttribute("class", "hidden");
+    summary.removeAttribute("class");
+    correct.textContent = `Correct Answers: ${finalScore}`;
+    total.textContent = `Total Questions: ${testPaper.length}`;
+    score.textContent = `Score ${finalScore}/${testPaper.length}`;
+    alert.textContent = grade ? "Congratulations!!!" : "Sorry!!!";
+    conclusion.textContent = grade
+      ? "You passed the test"
+      : "You failed the test";
+  };
+  finishButton.addEventListener("click", summaryFunction);
+
+  // take the user input and check whether the response is correct or incorrect or if the response has been provided or not
+  // also update the score and display the final score
+  let statMessage;
+  const ques = function (actualAnswer) {
+    const choices = document.getElementsByClassName("choice");
+    for (const selected of choices) {
+      if (selected.checked === true) {
+        flag = true;
+        const result =
+          selected.value === actualAnswer ? "correct" : "incorrect";
+        statMessage = `${selected.value} is ${result}`;
+        submitButton.setAttribute("disabled", "true");
+        if (result === "correct") {
+          card.setAttribute("class", "correct");
+          stat.textContent = statMessage;
+          finalScore++;
+        } else if (result === "incorrect") {
+          card.setAttribute("class", "incorrect");
+          stat.textContent = statMessage;
+        }
+        questionCounter++;
+        if (questionCounter < testPaper.length) {
+          nextButton.removeAttribute("disabled");
+        } else {
+          nextButton.setAttribute("class", "hidden");
+          finishButton.removeAttribute("disabled");
+          finishButton.removeAttribute("class");
+        }
+      }
+    }
+    if (!flag) {
+      stat.textContent = "Please select an option";
+    }
+  };
+}
+run();
+submitButton.addEventListener("click", function () {
+  ques(correctAnswer);
+});
